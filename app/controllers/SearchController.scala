78a8a205547d136470af8770d5185f62d87f95c7
Shumma-Tanaka <shumma_tanaka@dwango.co.jp>
2016-04-22 11:10:26 +0900

    length filter bugfix

--- app/controllers/SearchController.scala
+++ app/controllers/SearchController.scala
@@ -2,6 +2,7 @@ package controllers
 
 import com.google.inject.Inject
 import models.{Video, ContentInfo, Content, Param}
+import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
 import play.api.data.Form
 import play.api.data.Forms._
@@ -37,7 +38,7 @@ class SearchController @Inject() (ws: WSClient) extends Controller {
         } else {
           totalCount = -1
         }
-        Ok(views.html.index(params.getOrElse("q", "キーワードなし"), totalCount, videoList))
+        Ok(views.html.index(params, totalCount, videoList))
     }
   }
 
@@ -61,10 +62,10 @@ class SearchController @Inject() (ws: WSClient) extends Controller {
     "q" -> "ゲーム",
     "targets" -> "tagsExact",
     "fields" -> "contentId,title,tags,viewCounter,mylistCounter,commentCounter,startTime,thumbnailUrl",
-    "filters[startTime][gte]" -> "2016-04-01T00:00:00+09:00",
+    "filters[startTime][gte]" -> new DateTime().minusDays(30).toString("yyyy-MM-dd'T'HH:mm:ssZ"),
     "filters[lengthSeconds][gte]" -> "0",
-    "filters[lengthSeconds][lte]" -> "100",
-    "_sort" -> "viewCounter",
+    "filters[lengthSeconds][lte]" -> "6000",
+    "_sort" -> "-viewCounter",
     "_context" -> "nsearch")
   }
 
@@ -74,8 +75,8 @@ class SearchController @Inject() (ws: WSClient) extends Controller {
       case ("q", v) => params = params.updated("q", getParamsQuery(message.q).getOrElse(v))
       case ("targets", v) => params = params.updated("targets", getParamsTargets(message.t).getOrElse(v))
       case ("filters[startTime][gte]", v) => params = params.updated("filters[startTime][gte]", getParamsTimeFilters(message.r).getOrElse(v))
-      case ("filters[lengthSeconds][lte]", v) => params = params.updated("filters[lengthSeconds][lte]", getParamsLengthFilters(message.p1).getOrElse(v))
-      case ("filters[lengthSeconds][gte]", v) => params = params.updated("filters[lengthSeconds][gte]", getParamsLengthFilters(message.p2).getOrElse(v))
+      case ("filters[lengthSeconds][gte]", v) => params = params.updated("filters[lengthSeconds][gte]", getParamsLengthFilters(message.p1).getOrElse(v))
+      case ("filters[lengthSeconds][lte]", v) => params = params.updated("filters[lengthSeconds][lte]", getParamsLengthFilters(message.p2).getOrElse(v))
       case ("_sort", v) => params = params.updated("_sort", getParamsSort(message.s).getOrElse(v))
       case ("fields", v) =>
       case ("_context", v) =>
@@ -102,7 +103,7 @@ class SearchController @Inject() (ws: WSClient) extends Controller {
   }
 
   private def getParamsLengthFilters(p: Int): Option[String] = {
-    if (p < 0) None else Some(p.toString)
+    if (p < 0) None else Some((p*60).toString)
   }
 
   private def getParamsSort(s: String): Option[String] = {
